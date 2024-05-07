68e49fa7654112d0e077f201c028196fe6d76a8d
uxmangulzar <muhamadusmangulzar@gmail.com>
2024-05-08 04:05:31 +0500

    4th

--- lib/fitness_app/openAi/api_constants.dart
+++ lib/fitness_app/openAi/api_constants.dart
@@ -2,7 +2,7 @@
 const endpoint = 'https://api.openai.com/v1/';
 
 /// API key
-const apiToken = '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';
+const apiToken = '';
 
 /// Email for Gravatar image
-const emailAddress = 'farmanzia96@gmail.com';
+const emailAddress = '96@gail.com';
